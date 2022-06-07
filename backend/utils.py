aacdcb2c823427a104087a794dc17871eb445238
avyukd <avyukd@gmail.com>
2022-06-06 22:12:04 -0400

    multiple changes - NPV, TV, exit_TV - get_tickers, date parse - caching utility skeleton

--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,10 @@
 
-from typing import List
-
+from typing import Any, Dict, List
+import os
+from bs4 import BeautifulSoup
+from enums import Commodity
+import requests
+import re 
 
 def NPV(discount_rate: float, fcfs: List[float]) -> float:
     """
@@ -13,3 +17,56 @@ def TV(terminal_growth_rate: float, last_yr_fcf: float, discount_rate: float) ->
     Returns terminal value of a cash flow series.
     """
     return last_yr_fcf * (1 + terminal_growth_rate) / (discount_rate - terminal_growth_rate)
+
+def exit_TV(exit_multiple: float, last_yr_ebitda: float) -> float:
+    """
+    Returns terminal value based on exit multiple.
+    """
+    return last_yr_ebitda * exit_multiple
+
+def get_tickers():
+    """
+    Returns list of tickers of all companies in /stocks/*.py.
+    """
+    tickers = []
+    for filename in os.listdir("stocks"):
+        if filename[-3:] == ".py":
+            tickers.append(filename[:-3]) # remove .py
+    return tickers
+
+def cached(param: str) -> bool:
+    """
+    Returns True if param is in cache.
+    """
+    pass
+
+def load_param_from_cache(param: str) -> Any:
+    """
+    Assumes param is in cache. Loads the param. 
+    May have to wrap in object. Ex: CommodityCurve(RawCachedData)
+    """
+    pass
+
+
+
+def parse_date(date: str) -> str:
+    """
+    Returns date in format MM.YY. Given date in form "Aug'19".
+    """
+    year = date[-2:]
+    month = date[:-3]
+    month_dict = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
+    return month_dict[month] + "." + year
