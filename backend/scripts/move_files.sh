39f523008d1ffef6eac45d9e3042be8161d27f4f
avyukd <avyukd@gmail.com>
2022-06-22 21:08:45 -0400

    [auto] new files added

---
+++ backend/scripts/move_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv files/* ../../../../avyukd.github.io/files/*
+cd ../../../../avyukd.github.io
+git add .
+git commit -m "[auto] new files added"
+git push
+cd ../stocks/uip/backend/scripts
